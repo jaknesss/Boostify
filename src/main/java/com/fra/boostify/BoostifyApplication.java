711883466d413a709b02901ee816dc70d9555fc5
Francesco Venditti <97091210+jaknesss@users.noreply.github.com>
2025-04-24 10:40:38 +0200

    init commit

---
+++ src/main/java/com/fra/boostify/BoostifyApplication.java
@@ -0,0 +1,13 @@
+package com.fra.boostify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fra/boostify/BoostifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fra.boostify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
